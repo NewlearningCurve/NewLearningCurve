3b621666007ed291db90d87372f4e58b25338a9c
SHARMA <Payal.Sharma2@emc.com>
2020-09-20 13:21:28 +0530

    1st Commit

---
+++ myform.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.submit').click(function (event){
+        console.log('Clicked Button')
+
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var subject = $('.subject').val()
+        var message = $('.message').val()
+        statusElm.empty()
+
+        if (email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>Email is valid</div>')
+        }
+        else
+        {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid</div>')
+        }
+        if (subject.length > 5) {
+            statusElm.append('<div>Subject is valid</div>')
+        }
+        else
+        {
+            event.preventDefault()
+            statusElm.append('<div>Subject is not valid</div>')
+        }
+        if (message.length > 5 ){
+            statusElm.append('<div>Message is valid</div>')
+        }
+        else
+        {
+            event.preventDefault()
+            statusElm.append('<div>Message is not valid</div>')
+        }
+
+    });
+
+});
